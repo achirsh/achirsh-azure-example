100984fdeb62cea35399fcd64f11eb3639ccceec
葛青博 <geqb@sqbj.com>
2021-11-23 19:09:14 +0800

    no message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 export default function Index(): JSX.Element {
     return <div>
         <img src={require('../assets/blog/20181212211132.jpg')} />
-        <img src={require('../assets/blog/20181212214425.gif')} />
+        <img src={require('../assets/blog/O(1).gif')} />
+        <img src={require('../assets/blog/O(n).gif')} />
+        <img src={require('../assets/blog/O(logn).gif')} />
+        <img src={require('../assets/blog/O(n²).gif')} />
     </div>
 }
