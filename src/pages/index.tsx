c3242c8182347212937530631e985e5c60a4cb7d
葛青博 <geqb@sqbj.com>
2021-11-23 19:01:40 +0800

    no message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 export default function Index(): JSX.Element {
     return <div>
+        <img src={require('../assets/blog/20181212211132.jpg')} />
+        <img src={require('../assets/blog/20181212214425.gif')} />
     </div>
 }
